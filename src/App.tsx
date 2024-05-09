1280500b85667aac146c74a30fb345ca76a7f977
Ilya Katts <ilyak@wix.com>
2024-05-09 22:10:11 +0300

    Fix App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './App.module.scss';
 
 function App() {
-    return <div className={styles.App}></div>;
+    return <div className={styles.root}>App</div>;
 }
 
 export default App;
